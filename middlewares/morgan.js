6481a8d6a62366f937025cc32ea90fdf8d15bcbb
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2018-09-10 17:45:53 -0300

    Fix morgan middleware

--- middlewares/morgan.js
+++ middlewares/morgan.js
@@ -1,9 +1,13 @@
 'use strict'
 
+const env = require('sugar-env')
 const morgan = require('morgan')
+const merge = require('lodash.merge')
 
-const factory = ({ format, ...config }) => {
-  return morgan(format, config)
+const factory = ({ format, ...config }, environment) => {
+  const options = merge(config, { skip: () => { return environment !== env.TEST } })
+
+  return morgan(format, options)
 }
 
 module.exports = { factory }
